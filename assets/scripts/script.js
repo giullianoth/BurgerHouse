44f67cc375e5c584806486a475bce867735979c8
Giulliano <llthguimaraes@gmail.com>
2023-03-27 21:22:36 -0300

    mobile menu with js

---
+++ assets/scripts/script.js
@@ -0,0 +1,4 @@
+const menu = document.querySelector(".j_menu");
+const menuIcon = document.querySelector(".j_menu_icon");
+
+menuIcon.addEventListener("click", () => menu.classList.toggle("active"));
